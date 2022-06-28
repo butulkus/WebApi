ea15dee979cdd4a2a22363f4e8d24af166bf8170
butulkus <butulkus@gmail.com>
2022-06-28 21:24:14 +0300

    docker + compose

--- src/Catalog.Api/Program.cs
+++ src/Catalog.Api/Program.cs
@@ -56,7 +56,7 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (true/*app.Environment.IsDevelopment()*/)
 {
     app.UseSwagger(c =>
     {
